2e0090f2ad51961d954fafddadd6ef0043cb88d4
André Charton <acharton@ebay.com>
2021-02-10 22:32:41 +0100

    add todo, add comments

--- src/main/java/de/charton/reactive/PetShop.java
+++ src/main/java/de/charton/reactive/PetShop.java
@@ -7,16 +7,15 @@ public class PetShop {
 
   public static final String TOPPIC = "topping";
 
-
   public Mono<?> buyCatFood(Mono<String> cat) {
     return cat
         .map(it -> {
-          System.out.println("it -> addV");
+          System.out.println("it -> M says: addVitamin, now!");
           return addVitamin(it.length());
           }
         )
         .map(it -> {
-          System.out.println("flatMap");
+          System.out.println("use map, because I can.");
           return it;
         });
   }
@@ -28,6 +27,7 @@ public class PetShop {
           System.out.println("add toppic: " + it);
           return it.map(Mono::just).orElse(Mono.empty());
         });
+    // TODO get some fruits (apples, brokkoli, tomato, fenel form external provider!)
     return toppic.map(it -> size + " apples");
     // return size + " apples";
   }
